3a3ec20504f5f653e736271178985aff63ab328b
Gitoldor <rajguruowais@gmail.com>
2026-09-05 14:25:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -45,6 +45,18 @@ window.Sho1re1Places = [
     link: "welcome.html",
     plan: "FREE"
   },
+   {
+    id: 2,
+    title: "welcome",
+    topic: "Scenic Drives",
+    type: "",
+    subtype: "",
+    lat: 15.9041,
+    lng: 73.8219,
+    image: "https://i.ibb.co/chqGP3bQ/SAWANT-WADI-20260831-173946-0000.png",
+    link: "welcome.html",
+    plan: "FREE"
+  },
 
   {
     id: 3,
