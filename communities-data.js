4e821443b6183cb7bc2fc73e74f96290fc757971
Raj <u>
2026-09-10 19:09:10 +0530

    update

---
+++ communities-data.js
@@ -0,0 +1,44 @@
+window.communitiesData = [
+  {
+    id: "nba",
+    title: "NBA Threads",
+    image: "https://example.com/nba.png",
+    href: "nba.html"
+  },
+  {
+    id: "wnba",
+    title: "WNBA Threads",
+    image: "https://example.com/wnba.png",
+    href: "wnba.html"
+  },
+  {
+    id: "f1",
+    title: "F1 Threads",
+    image: "https://example.com/f1.png",
+    href: "f1.html"
+  },
+  {
+    id: "tennis",
+    title: "Tennis Threads",
+    image: "https://example.com/tennis.png",
+    href: "tennis.html"
+  },
+  {
+    id: "nfl",
+    title: "NFL Threads",
+    image: "https://example.com/nfl.png",
+    href: "nfl.html"
+  },
+  {
+    id: "football",
+    title: "Football Threads",
+    image: "https://example.com/football.png",
+    href: "football.html"
+  },
+  {
+    id: "cricket",
+    title: "Cricket Threads",
+    image: "https://example.com/cricket.png",
+    href: "cricket.html"
+  }
+];
