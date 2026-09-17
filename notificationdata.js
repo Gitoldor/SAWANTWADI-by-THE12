646e0bf6400257e5cdc73e6ea9a1023c56fba9ec
Gitoldor <rajguruowais@gmail.com>
2026-09-17 10:41:23 +0530

    Add notification n2

--- notificationdata.js
+++ notificationdata.js
@@ -32,4 +32,14 @@ window.Sho1re1Notifications = [
     link:"https://github.com/Gitoldor/SAWANTWADI-by-THE12",
     createdAt:"2026-09-17T05:10:19.535Z"
   }
+,
+
+  {
+    id:"n2",
+    image:"https://images.unsplash.com/photo-1500534623283-312aade485b7?auto=format&fit=crop&w=1200&q=80",
+    title:"Sex",
+    message:"Porm",
+    link:"https://images.unsplash.com/photo-1500534623283-312aade485b7?auto=format&fit=crop&w=1200&q=80",
+    createdAt:"2026-09-17T05:11:21.720Z"
+  }
 ];
