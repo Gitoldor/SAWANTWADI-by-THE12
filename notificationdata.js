8725b2027974b9b93862d020697fe1104015e259
Gitoldor <rajguruowais@gmail.com>
2026-09-17 10:40:21 +0530

    Add notification n1

--- notificationdata.js
+++ notificationdata.js
@@ -22,4 +22,14 @@ window.Sho1re1Notifications = [
 
   
 
-  ];
+  
+
+  {
+    id:"n1",
+    image:"https://images.unsplash.com/photo-1500534623283-312aade485b7",
+    title:"Yo",
+    message:"Yo",
+    link:"https://github.com/Gitoldor/SAWANTWADI-by-THE12",
+    createdAt:"2026-09-17T05:10:19.535Z"
+  }
+];
