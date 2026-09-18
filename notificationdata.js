bf05b99ddf6cbc729cf82d10c4f07068790b7c40
Gitoldor <rajguruowais@gmail.com>
2026-09-19 02:04:34 +0530

    Add notification n1

--- notificationdata.js
+++ notificationdata.js
@@ -1,3 +1,13 @@
 window.Sho1re1Notifications = [
 
-  ];
+  
+
+  {
+    id:"n1",
+    image:"[https://picsum.photos/800/600](https://picsum.photos/800/600?utm_source=chatgpt.com)",
+    title:"Yo cht",
+    message:"Yo",
+    link:"[https://picsum.photos/800/600](https://picsum.photos/800/600?utm_source=chatgpt.com)",
+    createdAt:"2026-09-18T20:34:31.921Z"
+  }
+];
